4a939b136254e780366a665ba2972b599d81f16a
ali bouhlel <alibouhlel96@gmail.com>
2018-07-31 15:57:02 +0100

    add logstash configuration

--- configuration.py
+++ configuration.py
@@ -3,6 +3,6 @@ consumer_key = ""
 consumer_secret = ""
 access_token = ""
 access_token_secret = ""
-#kafka configuration
+#kafka and zookeeper configuration
 KAFKA_TOPIC = 'your kafka topic'
 KAFKA_BROKERS = 'zookeeper host and port'
