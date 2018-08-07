846b5dc76003946d0110ca7c08bc07bada5951df
ali bouhlel <alibouhlel96@gmail.com>
2018-08-07 19:07:43 +0100

    update the script

--- start-kafka.sh
+++ start-kafka.sh
@@ -1,4 +1,6 @@
 #!/bin/bash  
-echo "Starting kafka and zookeeper"
 sudo kill `sudo lsof -t -i:2181`&sudo docker run -p  2181:2181 -p 9092:9092 -e ADVERTISED_HOST=127.0.0.1  johnnypark/kafka-zookeeper
+service logstash start
+service elasticsearch start
+service kibana start
 exit 
